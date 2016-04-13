266286ca9ba7b141fb3dfc9c905858036b862156
Andrew Odewahn <andrew@odewahn.com>
2016-04-13 15:07:57 -0400

    new repo

--- launchbot.rb
+++ launchbot.rb
@@ -1,7 +1,7 @@
 class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
-  url "https://github.com/launchbot/homebrew-client/releases/download/0.0.7/launchbot-0.0.7.tar.gz"
+  url "https://github.com/launchbot/homebrew-installers/releases/download/0.0.7/launchbot-0.0.7.tar.gz"
   sha256 "d1cb254049a819fabdb14ffd4bfb316c01b7ca44d06a99632e95238267ab7ed8"
 
   bottle :unneeded
