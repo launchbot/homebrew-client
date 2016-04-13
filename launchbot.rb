32e962779f18389702ff7821c0b9245b0f382bea
Andrew Odewahn <andrew@odewahn.com>
2016-04-13 13:48:52 -0400

    updated release url

--- launchbot.rb
+++ launchbot.rb
@@ -1,7 +1,7 @@
 class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
-  url "https://github.com/odewahn/homebrew-launchbot/releases/download/0.0.6/launchbot-0.0.6.tar.gz"
+  url "https://github.com/launchbot/homebrew-client/releases/download/0.0.6/launchbot-0.0.6.tar.gz"
   sha256 "7316e3510e24a3a4a119f0697718e50a2b095bb85d1dcfc48b90be110e4928d3"
 
   bottle :unneeded
