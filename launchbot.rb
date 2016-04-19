0dec0577f426a56ad7ca55839a9f8e7e712984ac
Andrew Odewahn <andrew@odewahn.com>
2016-04-19 13:46:05 -0400

    version bump to 0.0.8

--- launchbot.rb
+++ launchbot.rb
@@ -1,8 +1,8 @@
 class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
-  url "https://github.com/launchbot/homebrew-client/releases/download/0.0.7/launchbot-0.0.7.tar.gz"
-  sha256 "d1cb254049a819fabdb14ffd4bfb316c01b7ca44d06a99632e95238267ab7ed8"
+  url "https://github.com/launchbot/homebrew-client/releases/download/0.0.8/launchbot-0.0.8.tar.gz"
+  sha256 "5d83f8d16430bc96c1444c074c25f97a6a4659966ee9c460ad14903eb0d2a6ec"
 
   bottle :unneeded
 
