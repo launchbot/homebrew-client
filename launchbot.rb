648b5b3ea12f99fd5f5dbab139625f9ac0f22f4f
Andrew Odewahn <andrew@odewahn.com>
2016-07-25 15:47:35 -0400

    wrong version in release

--- launchbot.rb
+++ launchbot.rb
@@ -1,7 +1,7 @@
 class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
-  url "https://github.com/launchbot/homebrew-client/releases/download/0.1.3/launchbot-0.2.0.tar.gz"
+  url "https://github.com/launchbot/homebrew-client/releases/download/0.2.0/launchbot-0.2.0.tar.gz"
   sha256 "70b3bef8861f6faec5c9a0a2e6ae6e62f20a571c595fceed5756bc6e0c6a5edc"
 
   bottle :unneeded
